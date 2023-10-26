4a41047049ec1e51357c8bbf5bcf4a8b036d34a8
Bouh <bouh.abdillahi@polymtl.ca>
2023-10-26 10:22:11 -0400

    comparison monte-carlo with p4-4p3+4p2 added

--- pathset_mCarlo_test_failureCount.py
+++ pathset_mCarlo_test_failureCount.py
@@ -9,8 +9,8 @@ pathsets = [
 ]
 
 def simulate_packet_arrival(source, destination, pathsets, edge_probabilities, num_packets):
-    success_count = 0
-    success_rates = []
+    failure_count = 0  # Initialize the failure count
+    failure_rates = []
 
     for packet_number in range(1, num_packets + 1):
         success = False
@@ -29,22 +29,22 @@ def simulate_packet_arrival(source, destination, pathsets, edge_probabilities, n
                 success = True
             attempts += 1
 
-        if success:
-            success_count += 1
+        if not success:
+            failure_count += 1  # Increment failure count if the packet was not successfully transmitted
 
-        success_rate = success_count / packet_number
-        success_rates.append(success_rate)
+        failure_rate = failure_count / packet_number
+        failure_rates.append(failure_rate)
 
-    return success_rates
+    return failure_rates  # Return the failure rates
 
 source = 1  # source and destination match node labels
 destination = 4
-num_packets = 150000
+num_packets = 10000
 
 # Vary the success probability (probability_p) between 0 and 1
 success_probability_values = np.linspace(0, 1, num=101)  # 101 values from 0 to 1
 
-average_success_rates = []
+average_failure_rates = []
 
 for probability_p in success_probability_values:
     edge_probabilities = {
@@ -52,27 +52,22 @@ for probability_p in success_probability_values:
         (2, 4): probability_p,
         (1, 3): probability_p,
         (3, 4): probability_p
-        # (4, 5): probability_p,
-        # (5, 6): probability_p,
-        # (6, 8): probability_p,
-        # (5, 7): probability_p,
-        # (7, 8): probability_p
     }
-    success_rates = simulate_packet_arrival(source, destination, pathsets, edge_probabilities, num_packets)
-    average_success_rate = success_rates[-1]#sum(success_rates) / len(success_rates)
-    average_success_rates.append(average_success_rate)
+    failure_rates = simulate_packet_arrival(source, destination, pathsets, edge_probabilities, num_packets)
+    average_failure_rate = sum(failure_rates) / len(failure_rates)
+    average_failure_rates.append(average_failure_rate)
 
 # Calculate the values of the polynomial p^4 - 4p^3 + 4p^2 for the same range of success probabilities
 polynomial_values = success_probability_values**4 - 4 * success_probability_values**3 + 4 * success_probability_values**2
 
-# Plotting the success rates and the polynomial
-plt.plot(success_probability_values, average_success_rates, label="Packet Arrival Success Rate")
-plt.plot(success_probability_values, polynomial_values, label="p^4 - 4p^3 + 4p^2", linestyle='dashed')
+# Plotting the failure rates and the polynomial
+plt.plot(success_probability_values, average_failure_rates, label="Packet Arrival Failure Rate")
+plt.plot(success_probability_values, 1 - polynomial_values, label="1 - (p^4 - 4p^3 + 4p^2)", linestyle='dashed')
 plt.xlabel("Success Probability (probability_p)")
 plt.ylabel("Values")
-plt.title(f"Packet Arrival Success Rate vs. Polynomial Comparison for {num_packets} packets")
+plt.title(f"Packet Arrival Failure Rate vs. Polynomial Comparison for {num_packets} packets")
 plt.yscale('log')  # Set the y-axis to a logarithmic scale
 plt.legend()
 plt.grid(True)
-plt.savefig(f"monte-carlo_success_{num_packets}packets.png")
+plt.savefig(f"monte-carlo_failure_{num_packets}packets.png")
 # plt.show()
